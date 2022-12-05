ff5ddc2c4a99021ccbc9dc343a4e9dad226120ac
chostler24 <connor.hostler@gmail.com>
2022-12-05 14:29:25 -0600

    complete rework

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -7,29 +7,29 @@ import csv
 
 
 if __name__ == "__main__":
-    ID = argv[1]
-    API_URL = 'https://jsonplaceholder.typicode.com/'
-    url = API_URL + 'users/{}'.format(ID)
+    num_done, num_tasks = 0, 0
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
     user_response = requests.get(url)
-    profile = json.loads(user_response.text)
-    name = profile['username']
-    url_list = API_URL + 'todos/?userId={}'.format(ID)
-    todos_repsonse = requests.get(url_list)
-    todos_list = todos_repsonse.text
-    tasks = json.loads(todos_list)
-    tasks_done = []
-    tasks_done_count = 0
+    url = 'https://jsonplaceholder.typicode.com/todos/?userId={}'\
+        .format(user_id)
+    todo_response = requests.get(url)
+    user_info = json.loads(user_response.text)
+    todo_info = json.loads(todo_response.text)
 
-    for task in tasks:
+    task_list = []
+    username = user_info['username']
+    for task in todo_info:
         task_dict = {}
-        ID = task_dict['USER_ID']
-        name = task_dict['USERNAME']
-        task['completed'] = task_dict['TASK_COMPLETED_STATUS']
-        task['title'] = task_dict['TASK_TITLE']
-        tasks_done.append(task_dict)
+        task_dict['USER_ID'] = user_id
+        task_dict['USERNAME'] = username
+        task_dict['TASK_COMPLETED_STATUS'] = task['completed']
+        task_dict['TASK_TITLE'] = task['title']
+        task_list.append(task_dict)
 
     fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-    with open('./{}.csv'.format(ID), 'w', encoding='UTF-8', newline='') as f:
+    with open('./{}.csv'.format(user_id), 'w', encoding='UTF8',
+              newline='') as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames,
                                 quoting=csv.QUOTE_ALL, quotechar='"')
-        writer.writerows(tasks_done)
+        writer.writerows(task_list)

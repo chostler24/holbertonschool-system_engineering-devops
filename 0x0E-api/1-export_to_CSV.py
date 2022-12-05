2950ef0c4a051a0f2fbf8cab93ad3dd58423124d
chostler24 <connor.hostler@gmail.com>
2022-12-05 13:50:45 -0600

    task 1

---
+++ 0x0E-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""module"""
+import json
+import requests
+from sys import argv
+import csv
+
+
+if __name__ == "__main__":
+    ID = argv[1]
+    API_URL = 'https://jsonplaceholder.typicode.com/'
+    url = API_URL + 'users/{}'.format(ID)
+    user_response = requests.get(url)
+    profile = json.loads(user_response.text)
+    name = profile['name']
+    url_list = API_URL + 'todos/?userId={}'.format(ID)
+    todos_repsonse = requests.get(url_list)
+    todos_list = todos_repsonse.text
+    tasks = json.loads(todos_list)
+    tasks_done = []
+    tasks_done_count = 0
+
+    fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+    with open('./{}.csv'.format(ID), 'w', encoding='UTF-8', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL, quotechar='"')
+        writer.writerows(tasks_done)

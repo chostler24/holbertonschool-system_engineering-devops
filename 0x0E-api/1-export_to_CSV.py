12ba53fcce2e5e61fe73b55d860cde39fe037c15
chostler24 <connor.hostler@gmail.com>
2022-12-05 13:52:08 -0600

    changed id variable name

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -7,13 +7,13 @@ import csv
 
 
 if __name__ == "__main__":
-    ID = argv[1]
+    USER_ID = argv[1]
     API_URL = 'https://jsonplaceholder.typicode.com/'
-    url = API_URL + 'users/{}'.format(ID)
+    url = API_URL + 'users/{}'.format(USER_ID)
     user_response = requests.get(url)
     profile = json.loads(user_response.text)
     name = profile['name']
-    url_list = API_URL + 'todos/?userId={}'.format(ID)
+    url_list = API_URL + 'todos/?userId={}'.format(USER_ID)
     todos_repsonse = requests.get(url_list)
     todos_list = todos_repsonse.text
     tasks = json.loads(todos_list)
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     tasks_done_count = 0
 
     fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-    with open('./{}.csv'.format(ID), 'w', encoding='UTF-8', newline='') as f:
+    with open('./{}.csv'.format(USER_ID), 'w', encoding='UTF-8', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames,
                                 quoting=csv.QUOTE_ALL, quotechar='"')
         writer.writerows(tasks_done)

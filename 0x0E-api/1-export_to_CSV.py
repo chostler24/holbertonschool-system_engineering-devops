45a4d99591a74af054141f0d9b26bd574cb018c1
chostler24 <connor.hostler@gmail.com>
2022-12-05 14:05:30 -0600

    changed task block

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -7,13 +7,13 @@ import csv
 
 
 if __name__ == "__main__":
-    USER_ID = argv[1]
+    ID = argv[1]
     API_URL = 'https://jsonplaceholder.typicode.com/'
-    url = API_URL + 'users/{}'.format(USER_ID)
+    url = API_URL + 'users/{}'.format(ID)
     user_response = requests.get(url)
     profile = json.loads(user_response.text)
     name = profile['name']
-    url_list = API_URL + 'todos/?userId={}'.format(USER_ID)
+    url_list = API_URL + 'todos/?userId={}'.format(ID)
     todos_repsonse = requests.get(url_list)
     todos_list = todos_repsonse.text
     tasks = json.loads(todos_list)
@@ -21,12 +21,15 @@ if __name__ == "__main__":
     tasks_done_count = 0
 
     for task in tasks:
-        if task.get('completed'):
-            tasks_done.append(task)
-            tasks_done_count += 1
+        task_dict = {}
+        user_id = task_dict['USER_ID']
+        username = task_dict['USERNAME']
+        task['completed'] = task_dict['TASK_COMPLETED_STATUS']
+        task['title'] = task_dict['TASK_TITLE']
+        tasks_done.append(task_dict)
 
     fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-    with open('./{}.csv'.format(USER_ID), 'w', encoding='UTF-8', newline='') as f:
+    with open('./{}.csv'.format(user_id), 'w', encoding='UTF-8', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames,
                                 quoting=csv.QUOTE_ALL, quotechar='"')
         writer.writerows(tasks_done)
